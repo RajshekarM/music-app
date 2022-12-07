cf7b64dbcb0afd8bb2984223f558c2314f1c7a60
rajshekar <rajshekar.mudhigonda2468@gmail.com>
2022-12-07 08:50:19 +0530

    adding environment variables npm install dotenv:

--- server/express/index.js
+++ server/express/index.js
@@ -1,9 +1,16 @@
+import dotnev from 'dotenv';
 import express from 'express';
 import  request  from 'request';
+
+//configuring environment variables
+dotnev.config();
+
 
 //this will create the server and start the server
 const app = express();
 const PORT = 8080;
+
+console.log(process.env.CLIENT_ID);
 
 app.get('/',(req,res)=>{res.send('hello working')});
 
